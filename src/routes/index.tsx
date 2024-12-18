0d6fa7ab22f9852b6368f4989cc6fb028ad95cd3
gui <xdgrxd.dev@gmail.com>
2024-12-17 21:45:48 -0300

    feat: mui add

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<p>Home page</p>} />
+      <Route path="/" element={<Button>Test</Button>} />
       <Route path="*" element={<p>Error 404</p>} />
     </Routes>
   );
